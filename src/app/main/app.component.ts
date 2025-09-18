0d57b110436c62809efffb91e56ba21539b95cad
luccabibar <luccabibar@gmail.com>
2025-09-17 21:46:49 -0300

    redisposicao componente principal

--- src/app/main/app.component.ts
+++ src/app/main/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet, HeaderComponent, ContentComponent, FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
