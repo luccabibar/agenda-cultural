0023f0ea8503a3e69958f4976a11eea10bfb60e3
luccabibar <luccabibar@gmail.com>
2025-10-12 19:43:42 -0300

    login service agora guarda dados em cookies; trabalhando em permanencia

--- src/services/login-service/login.service.ts
+++ src/services/login-service/login.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { UsuarioAutenticado } from '../../interfaces/usuario/usuairo-autenticado';
+import { AgendaCulturalService } from '../agenda-cultural-service/agenda-cultural.service';
+import { Resposta } from '../../interfaces/resposta';
+import { HttpResponse } from '@angular/common/http';
+import { Usuario } from '../../interfaces/usuario/usuarios';
+import { CookieService } from 'ngx-cookie-service'
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +15,65 @@ export class LoginService
   private usuario: UsuarioAutenticado | null;
   private subject: Subject<UsuarioAutenticado | null>;
 
+  private cookieKey: string = 'STOREDTOKEN';
+  private storedToken: string | null
 
-  constructor()
-  {
+  constructor(
+    private acService: AgendaCulturalService,
+    private cookieService: CookieService
+  ) {
     this.subject = new Subject<UsuarioAutenticado | null>();
     this.usuario = null; // source from sstorage
 
-    this.setUsuario(null);
+    // busca sessao anterior, tenta autenticar 
+    this.storedToken = cookieService.check(this.cookieKey) ? cookieService.get(this.cookieKey) : null;
+    
+    // tenta restaurar sessao (verifica se token eh valida)
+    if(this.storedToken){
+      // usuario autenticado dummy
+      let fauxUser =  UsuarioAutenticado.of({
+        usuario: null,
+        authToken: this.storedToken
+      } as UsuarioAutenticado);
+
+      acService.userDados(fauxUser).subscribe({
+        next: this.authNext,
+        error: this.authError
+      });
+    }
+    // nova sessao
+    else{
+      console.log("Nao havia usuario logado");
+      this.logout();
+    }
   }
+  
+  
+  authNext = (res: Resposta<Usuario>): void =>
+  {
+    if(!res.response){
+      this.authError("Erro ao reaver sessao (token expirada?) " + res);
+    }
+    else{
+      let user = UsuarioAutenticado.of({
+        usuario: res.response,
+        authToken: this.storedToken
+      } as UsuarioAutenticado);
+
+      this.login(user);
+    }
+  }
+    
+    
+  authError = (res: HttpResponse<unknown> | string): void =>
+  {
+    if (res)
+      console.log(res);
+
+    // nova sessao
+    this.logout();
+  }
+
 
   getUsuario(): UsuarioAutenticado | null { return this.usuario; }
   getSubject(): Subject<UsuarioAutenticado | null> { return this.subject; }
@@ -25,6 +81,21 @@ export class LoginService
 
   private setUsuario(user: UsuarioAutenticado | null)
   {
+    // set nova sessao
+    if(user && user.authToken)
+      this.cookieService.set(
+        this.cookieKey, // Key
+        user.authToken, // Value
+        86400000, // TTL (1 dia)
+        undefined, // Path
+        undefined, // Domain
+        false,// Secure (SSL) (TODO: habilitar SSL)
+        'Strict'// SameSite policy
+      );
+    //unset sessao
+    else
+      this.cookieService.delete(this.cookieKey);
+
     this.usuario = user;
     this.subject.next(this.usuario);
   }
@@ -42,10 +113,7 @@ export class LoginService
   
   logout(): boolean
   {
-    if(!this.usuario)
-      return false;
-
     this.setUsuario(null);
-    return false;
+    return true;
   }
 }
