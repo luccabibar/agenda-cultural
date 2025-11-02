c00fccb2a4af5fd6319f02f0d6c97578e87163b9
luccabibar <luccabibar@gmail.com>
2025-11-02 19:34:00 -0300

    busca na barra de pesquisa

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,15 +1,40 @@
 import { Component } from '@angular/core';
 import { UsuarioHeaderComponent } from './login-header/usuario-header.component';
-import { RouterLink } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
+import { BuscarDados } from '../../../interfaces/buscar';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [UsuarioHeaderComponent, RouterLink],
+  imports: [UsuarioHeaderComponent, RouterLink, FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent 
 {
+  textoBusca: string;
 
+  constructor(
+    private router: Router
+  ) {
+    this.textoBusca = "";
+  }
+
+
+  buscar()
+  {
+    let texto = this.textoBusca.trim();  
+
+    if(texto.length <= 0){
+      this.router.navigate(['/buscar']);
+      return;
+    }
+    else{
+      let filtros = new BuscarDados();
+      filtros.texto = texto;
+
+      this.router.navigate(['/buscar'], { queryParams: filtros })
+    }
+  }
 }
